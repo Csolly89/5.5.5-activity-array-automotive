ef2b8434fb683fdd4250b52be67808a74265d2e2
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-09-11 18:14:27 -0400

    vehicle finish

--- vehicle.js
+++ vehicle.js
@@ -1,4 +1,24 @@
 class Vehicle {
+    constructor(make,model,year,price,vin){
+        this.model = model;
+        this.paint = make;
+        this.year = year;
+        this.price = price;
+        this.vin = vin;
+        this.color = undefined;
+    }
+    paint(color){
+        this.color = color
+    }
+    drive(){
+        return this.year + " " + this.make + " " + this.model + "drives"
+    }
+        
     
 }
 
+class Truck extends Vehicle {
+    constructor(){
+        super (make,model,price,vin)
+}
+}
