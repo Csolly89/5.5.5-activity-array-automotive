d72755aac59c3950bb5c757eaa66e58593ccb686
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-09-12 17:26:55 -0400

    both test classes passed

--- factory.js
+++ factory.js
@@ -1,12 +1,12 @@
 class Factory{
     constructor(location,owner){
-        this.location = location;
-        this.owner = owner;
-        this.vehicles = [];
+        this.location = location
+        this.owner = owner
+        this.vehicles = []
     }
 
     createVehicle(vehicle){
-        this.vehicles.push(vehicles)
+        this.vehicles.push(vehicle)
         }
     
 
--- vehicle.js
+++ vehicle.js
@@ -1,19 +1,19 @@
 class Vehicle {
     constructor(make,model,year,price,vin){
-        this.model = model;
-        this.paint = make;
-        this.year = year;
-        this.price = price;
-        this.vin = vin;
-        this.color = undefined;
+        this.make = make
+        this.model = model
+        this.year = year
+        this.price = price
+        this.vin = vin
+        this.color = undefined
     }
+
     paint(color){
         this.color = color
     }
+
     drive(){
-        return this.year + " " + this.make + " " + this.model + "drives"
+        return this.year + " " + this.make + " " + this.model + " " + "drives" 
     }
-        
-    
 }
 
